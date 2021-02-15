11753098482f1ad63f7dbd4d8a06b25e5e89fa75
candrepa1 <candrepa1@gmail.com>
2021-02-15 08:20:01 -0500

    keys console.log

--- src/components/LocationInfo.js
+++ src/components/LocationInfo.js
@@ -5,7 +5,6 @@ import ResidentContainer from './ResidentContainer';
 
 const LocationInfo = (props) => {
     const info = props.location;
-    console.log(info);
     const list = info.map((item) => <><li key="0"><b>Name:</b> {item.name}</li><li key="1"><b>Type:</b> {item.type}</li><li key="2"><b>Dimension:</b> {item.dimension}</li><li key="3"><b>Number of Residents:</b> {item.residents.length}</li></>);
 
     return(
